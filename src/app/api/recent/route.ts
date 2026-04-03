8ac0fef16c3688f160b4ab93fa57a5ad7a1e05e9
MG <mg@MGs-MacBook-Pro.local>
2026-04-02 21:37:24 -0700

    feat: increase recent songs list from 3 to 10

--- src/app/api/recent/route.ts
+++ src/app/api/recent/route.ts
@@ -24,7 +24,7 @@ export async function GET(): Promise<Response> {
       "fields[]": "song_title",
     });
     // Airtable doesn't support multiple fields[] in URLSearchParams easily
-    const url = `https://api.airtable.com/v0/${baseId}/Conversions?maxRecords=3&sort%5B0%5D%5Bfield%5D=last_searched&sort%5B0%5D%5Bdirection%5D=desc&fields%5B%5D=song_title&fields%5B%5D=artist&fields%5B%5D=spotify_link&fields%5B%5D=apple_music_link&fields%5B%5D=youtube_link`;
+    const url = `https://api.airtable.com/v0/${baseId}/Conversions?maxRecords=10&sort%5B0%5D%5Bfield%5D=last_searched&sort%5B0%5D%5Bdirection%5D=desc&fields%5B%5D=song_title&fields%5B%5D=artist&fields%5B%5D=spotify_link&fields%5B%5D=apple_music_link&fields%5B%5D=youtube_link`;
 
     const res = await fetch(url, {
       headers: { Authorization: `Bearer ${token}` },
